ac570aa16be753652aaab53013ac7a9cfcfe9eb5
Shadrach Dameteye <shadyclay21@gmail.com>
2022-06-19 03:17:24 +0100

    make it better latest

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client';
 import React from 'react';
 // const SOCKET_URL = 'http://localhost:5001';
 const SOCKET_URL = 'http://zibrachat-api.herokuapp.com/';
+
 export const socket = io(SOCKET_URL);
 // app context
 export const AppContext = React.createContext();
